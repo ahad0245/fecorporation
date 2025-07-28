e2008917175019482edb54eff97bd58772395c4a
ahad0245 <abdulahad@i8is.com>
2025-07-28 17:20:31 +0500

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
-  },
+  }
 })
-
