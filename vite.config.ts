cddfd164b7a38ef605415942ce25527876529544
ahad0245 <abdulahad@i8is.com>
2025-07-28 17:29:54 +0500

    update fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     emptyOutDir: true,
     rollupOptions: {
       input: {
